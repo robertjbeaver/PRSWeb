b3b8661da569687844ba674fe87779a38ee58a8f
Robert Beaver <robertjbeaver@gmail.com>
2018-04-11 10:54:01 -0400

    Initial commit

---
+++ src/main/java/com/prs/business/user/User.java
@@ -0,0 +1,128 @@
+package com.prs.business.user;
+
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String userName;
+	private String password;
+	private String firstName;
+	private String lastName;
+	@Column(name = "phone")
+	private String phoneNumber;
+	private String email;
+	@Column(name = "isReviewer")
+	private boolean reviewer;
+	@Column(name = "isAdmin")
+	private boolean admin;
+	@Transient
+	private LocalDateTime dateCreated;
+	
+	public User() {
+		userName = "";
+		password = "";
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firtName) {
+		this.firstName = firtName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public boolean isReviewer() {
+		return reviewer;
+	}
+	public void setReviewer(boolean reviewer) {
+		this.reviewer = reviewer;
+	}
+	public boolean isAdmin() {
+		return admin;
+	}
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+	public User(int id, String userName, String password, String firtName, String lastName, String phoneNumber,
+			String email, boolean reviewer, boolean admin) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.password = password;
+		this.firstName = firtName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.reviewer = reviewer;
+		this.admin = admin;
+	}
+	public User(String userName, String password, String firtName, String lastName, String phoneNumber,
+			String email, boolean reviewer, boolean admin) {
+		super();
+		this.userName = userName;
+		this.password = password;
+		this.firstName = firtName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.reviewer = reviewer;
+		this.admin = admin;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", userName=" + userName + ", password=" + password + ", firstName=" + firstName
+				+ ", lastName=" + lastName + ", phoneNumber=" + phoneNumber + ", email=" + email + ", reviewer="
+				+ reviewer + ", admin=" + admin + "]";
+	}
+	
+	
+	
+}
